ede4b6a36b66b8d931ae5f6c87fc83385ad33c91
masanov3167 <masanov3167@gmail.com>
2023-01-28 18:57:50 +0500

    react reveal added

--- src/components/animateCard/index.js
+++ src/components/animateCard/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Fade from 'react-reveal/Fade';
 
 const AnimateCard = ({animClass, animTime,refLink, refTitle, refId, key, mainTitle}) =>{
     return(
-        <Link className={`facultetTitle wow ${animClass || ''}`} data-wow-delay={animTime} to={`/${refLink}/${refTitle.toLowerCase().split(' ').map(str => str.split('').filter(char => /[a-zA-Z]/.test(char)).join('')).join('-')}-${refId}`} key={key}>
+        <Fade right>
+            <Link className={`facultetTitle `}  to={`/${refLink}/${refTitle.toLowerCase().split(' ').map(str => str.split('').filter(char => /[a-zA-Z]/.test(char)).join('')).join('-')}-${refId}`} key={key}>
                 <h3 className="fakultet-inner">{mainTitle}</h3>
-        </Link>
+            </Link>
+        </Fade>
     )
 }
 
